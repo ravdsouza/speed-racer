ee5b3f972da90f20192fd732aaf553912b2fe75e
Raveena <ravadsouza@gmail.com>
2018-07-02 18:23:37 -0400

    July 2 - delete old conig project

--- Project4/retarget.d
+++
@@ -1,5 +0,0 @@
-retarget.o: Retarget.c
-retarget.o: c:\Software\Keil\ARM\ARMCC\bin\..\include\stdio.h
-retarget.o: c:\Software\Keil\ARM\ARMCC\bin\..\include\rt_misc.h
-retarget.o: uart.h
-retarget.o: c:\Software\Keil\ARM\ARMCC\bin\..\include\stdint.h
